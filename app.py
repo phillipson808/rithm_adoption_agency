64b6c1bf4bc2fbfc3f3420d1e2e63d5bff0848db
Austin <austin.yau@gmail.com>
2019-04-19 15:59:40 -0700

    boom- built page with petfinder api

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
-from forms import AddPetForm
+from forms import AddPetForm, EditPetForm
+from secrets import ACCESS_TOKEN
+import requests
 
 app = Flask(__name__)
 
@@ -17,7 +19,25 @@ db.create_all()
 @app.route('/')
 def home_page():
     pets = Pet.query.all()
-    return render_template('home.html', pets=pets)
+
+    # headers = {'Authorization': f'Bearer {ACCESS_TOKEN}'}
+
+    resp = requests.get(
+            "https://api.petfinder.com/v2/animals", 
+            headers={'Authorization': f'Bearer {ACCESS_TOKEN}'}
+       )
+
+    response = resp.json()
+
+    animals = response["animals"]
+
+
+    pf_name = animals[0]["name"]
+    pf_photo = animals[0]["photos"][0]["medium"]
+    pf_age = animals[0]["age"]
+
+
+    return render_template('home.html', pets=pets, pf_name=pf_name, pf_photo=pf_photo, pf_age=pf_age)
 
 @app.route('/add', methods=["GET", "POST"])
 def add_pet():
@@ -38,4 +58,24 @@ def add_pet():
     else:
         return render_template('add-pet.html', form=form)
 
+@app.route('/<pet_id>', methods=["GET", "POST"])
+def show_pet_profile(pet_id):
+    pet = Pet.query.get(pet_id)
+
+    edit_form = EditPetForm(obj=pet)
+
+    if edit_form.validate_on_submit():
+
+        pet.photo_url = edit_form.photo_url.data
+        pet.notes = edit_form.notes.data
+        pet.available = edit_form.available.data
+
+        db.session.add(pet)
+        db.session.commit()
+        return redirect(f"/{pet_id}")
+
+    else:
+        return render_template('pet-profile.html', pet=pet, edit_form=edit_form)
+
+    
 
--- forms.py
+++ forms.py
@@ -1,12 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, SelectField
-from wtforms.validators import InputRequired, Optional, Email, AnyOf
+from wtforms import StringField, IntegerField, TextAreaField, SelectField, BooleanField
+from wtforms.validators import InputRequired, Optional, Email, AnyOf, NumberRange, URL
 from flask_debugtoolbar import DebugToolbarExtension
 
 class AddPetForm(FlaskForm):
     name = StringField("Pet Name", validators=[InputRequired()])
     species = SelectField("Species", choices=[('dog', 'Dog'), ('porcupine', 'Porcupine'), ('cat', 'Cat')], validators=[InputRequired()])
-    photo_url = StringField("Photo URL", validators=[Optional()])
-    age = IntegerField("Age", validators=[InputRequired()])
+    photo_url = StringField("Photo URL", validators=[Optional(), URL(require_tld=True)])
+    age = IntegerField("Age", validators=[InputRequired(), NumberRange(0, 30)])
     notes = TextAreaField("Notes", validators=[Optional()])
+    
 
+class EditPetForm(FlaskForm):
+    photo_url = StringField("Photo URL", validators=[Optional(), URL(require_tld=True)])
+    notes = TextAreaField("Notes", validators=[Optional()])
+    available = BooleanField("Available")
+    
